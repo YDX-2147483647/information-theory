1bba65dd199768a3b90dc399523003be32e5b7a2
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2022-11-22 15:28:24 +0800

    [jpeg] `main_hack` about run length encoding

--- jpeg/src/main_hack.m
+++ jpeg/src/main_hack.m
@@ -26,3 +26,21 @@ fprintf('量化后，非零元素只占 %.1f%%。\n', mean(logical(data), 'all')
 
 imagesc(log(mean(abs(data), 3)));
 title('量化后（按对数上色）');
+input('继续？> ');
+
+
+%% 游程编码
+data = zigzag_destruct(data);
+data = serialize(data);
+fprintf("游程编码后，数据量从 %.1f kiB 降到了 %.1f kiB，仅为 %.1f%%。\n", ...
+    whos('img').bytes / 1024, ...
+    whos('data').bytes / 1024, ...
+    whos('data').bytes / whos('img').bytes * 100 ...
+);
+fprintf('编码后数据分布仍不均匀，而且是两种分布混合（零附近的 AC 与 +60 附近的 DC）。\n');
+fprintf('总之仍有进一步压缩空间。\n');
+histogram(data);
+title('游程编码后数据');
+xlabel('数据取值');
+ylabel('频数/组距');
+
