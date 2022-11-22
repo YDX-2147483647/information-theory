3c7e6c562067f6a4a7f81cc00f62802c8402ff07
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2022-11-22 21:01:51 +0800

    [jpeg] 计算码率和失真

--- jpeg/src/main_hack.m
+++ jpeg/src/main_hack.m
@@ -37,10 +37,25 @@ fprintf("游程编码后，数据量从 %.1f kiB 降到了 %.1f kiB，仅为 %.1
     whos('data').bytes / 1024, ...
     whos('data').bytes / whos('img').bytes * 100 ...
 );
+
 fprintf('编码后数据分布仍不均匀，而且是两种分布混合（零附近的 AC 与 +60 附近的 DC）。\n');
-fprintf('总之仍有进一步压缩空间。\n');
 histogram(data);
 title('游程编码后数据');
 xlabel('数据取值');
 ylabel('频数/组距');
 
+prob = histcounts(data, -68:66) / length(data);
+% log(0) = -inf, 0 * inf = NaN.
+r = sum(rmmissing(- prob .* log2(prob)));
+fprintf("若当成无记忆信源，按频率估计概率，则码率只有 %.2f bits/sig，效率仅为 %.1f%%。\n", ...
+    r, r / 8 * 100 ...
+);
+
+fprintf('总之仍有进一步压缩空间。\n');
+
+
+%% 失真
+img_recovered = decode_img(data, size(img), Q);
+delta = img_recovered - img;
+mean_squared_error = mean(delta .^ 2, 'all');
+fprintf('均方误差失真为 %.2f。\n', mean_squared_error);
